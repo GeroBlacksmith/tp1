2b687f415149b515e9134ce1bf51e0f4b6365508
Gerardo Herrera <GeroBlacksmith@users.noreply.github.com>
2016-03-22 23:46:46 -0300

    Tabs without jquery plugins

---
+++ index (copy).php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link href='estilo.css' rel='stylesheet' type="text/css">
+    </head>
+    <body>
+        <div id='content'>
+            <ul id='tabs'>
+                <li id='tab-1' class='selected'  >Tab 1</li>
+                <li id='tab-2' class='noselected'>Tab 2</li>
+                <li id='tab-3' class='noselected'>Tab 3</li>
+            </ul>
+            <div id='tab-content'>
+
+            </div>
+        </div>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+        <script src="jquery-2.2.1.min.js"></script>
+        <script src="scrtabmanager.js"></script>
+    </body>
+</html>
